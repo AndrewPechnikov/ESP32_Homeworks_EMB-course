96fda91dbdd54bc46d00114faace5c3b70430184
Pechnikov <andrij.pechnikov@gmail.com>
2026-01-31 12:21:16 +0200

    third iteration

--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,12 @@
 
 
 #define LED_RED_PIN 15
-#define LED_BLUE_PIN 2
+#define LED_BLUE_PIN 3
 #define LED_GREEN_PIN 4
 
-bool flagOnePIN = 0;
-bool flagAdvanceBlink = 1;
+#define LED_TWO_PIN 2
+
+
 
 void advanced_blinking(int LED1, int LED2, int LED3){
   for(int i = 0; i < 4; i++){
@@ -42,11 +43,14 @@ void advanced_blinking(int LED1, int LED2, int LED3){
 void blinking(int LED1, int LED2, int LED3){
 
   digitalWrite(LED1, 1);
-  digitalWrite(LED2, 0);
   delay(1000);
   digitalWrite(LED1, 0);
   digitalWrite(LED2, 1);
   delay(1000);
+  digitalWrite(LED2, 0);
+  digitalWrite(LED3, 1);
+  delay(1000);
+  digitalWrite(LED3, 0);
   
 }
 
@@ -55,6 +59,7 @@ void setup() {
   pinMode(LED_RED_PIN, OUTPUT);
   pinMode(LED_BLUE_PIN, OUTPUT);
   pinMode(LED_GREEN_PIN, OUTPUT);
+  pinMode(LED_TWO_PIN, OUTPUT);
 }
 
 
@@ -63,19 +68,18 @@ void loop() {
 
   
 
-  if(!flagOnePIN && flagAdvanceBlink){
-    
-    advanced_blinking(LED_BLUE_PIN, LED_RED_PIN, LED_GREEN_PIN);
-  }
-  else if (flagOnePIN){
-    digitalWrite(LED_RED_PIN, 1);
-    delay(1000);
-    digitalWrite(LED_RED_PIN, 0);
-    delay(1000);
-  }
-  else {
-    blinking(LED_BLUE_PIN, LED_RED_PIN, LED_GREEN_PIN);
-  }
+
+  advanced_blinking(LED_BLUE_PIN, LED_RED_PIN, LED_GREEN_PIN);
+  
+  
+  digitalWrite(LED_TWO_PIN, 1);
+  delay(1000);
+  digitalWrite(LED_TWO_PIN, 0);
+  delay(1000);
+  
+  
+  blinking(LED_BLUE_PIN, LED_RED_PIN, LED_GREEN_PIN);
+  
 
 }
 
