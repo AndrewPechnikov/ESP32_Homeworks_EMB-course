f44b9aaadfe6c6d69fb6d36bca9c1c33806bdf27
А.О. Печников <a.pechnikov@radiy.com>
2026-02-02 14:20:14 +0200

    Second iteration

--- src/main.cpp
+++ src/main.cpp
@@ -1,97 +1,125 @@
 #include <Arduino.h>
 
-
 #define LED_RED_PIN 16
 
 #define LED_GREEN_PIN 4
 
 #define FIRST_BUTTON_PIN 21
 #define SECOND_BUTTON_PIN 0
-int mode;
+
+#define SHORT_TIME_PAUSE 100
+#define LONG_TIME_PAUSE 1000
+
+#define DEBOUNCE_TIME 50
+
+/*0 - повільне перемикання 
+1 - швидке перемикання 
+2 - одночасне перемикання*/
+char mode = 0;
+
+/*0 - не має закільцьованого перемикання 
+1 - є закільцьоване перемикання з повернення на mode 0 при одночасному натисканні */
+const bool SERIAL_MODE = 0;
 
 
-void fast_blinking(int LED1, int LED2){
-  for(int i = 0; i < 4; i++){
-    digitalWrite(LED1, 1);
-    delay(100);
-    digitalWrite(LED1, 0);
-    delay(100);
-    digitalWrite(LED2, 1);
-    delay(100);
-    digitalWrite(LED2, 0);
-    delay(100);
-  }
+void switchLedLogika(char mode);
+void fast_blinking(int LED1, int LED2);
+void blinking(int LED1, int LED2);
+void alarm(int LED1, int LED2);
 
 
-}
-
-void blinking(int LED1, int LED2){
-
-  digitalWrite(LED1, 1);
-  delay(1000);
-  digitalWrite(LED1, 0);
-  digitalWrite(LED2, 1);
-  delay(1000);
-  digitalWrite(LED2, 0);
-}
-
-void alarm(int LED1, int LED2){
-
-     digitalWrite(LED1, 1);
-    digitalWrite(LED2, 1);
-    delay(1000);
-    digitalWrite(LED1, 0);
-    digitalWrite(LED2, 0);
-    delay(1000);
-}
-
-void setup() {
+void setup()
+{
 
   pinMode(LED_RED_PIN, OUTPUT);
   pinMode(LED_GREEN_PIN, OUTPUT);
 
   pinMode(FIRST_BUTTON_PIN, INPUT);
-  pinMode(FIRST_BUTTON_PIN, INPUT_PULLUP);
-
-
-  
+  pinMode(SECOND_BUTTON_PIN, INPUT_PULLUP);
 }
 
-
-
-void loop() {
+void loop()
+{
 
   bool firstButton = digitalRead(FIRST_BUTTON_PIN);
-  bool secondButton = digitalRead(SECOND_BUTTON_PIN);
+  bool secondButton = digitalRead(!SECOND_BUTTON_PIN);
 
-  
 
-  mode = (firstButton) ? 1 : mode;
-  mode = (!secondButton) ? 2 : mode;
-  mode = (!secondButton && firstButton) ? 0 : mode;
-  delay(1000);
-  if (mode == 0){
-    blinking(LED_RED_PIN, LED_GREEN_PIN);
-  }
-  else if (mode == 1)
-  {
-    fast_blinking(LED_RED_PIN, LED_GREEN_PIN);
-  }
+  if(!SERIAL_MODE){
 
-  else if (mode == 2)
-  {
-    
-    alarm(LED_RED_PIN, LED_GREEN_PIN);
+    mode = (firstButton) ? 1 : mode;
+    mode = (secondButton) ? 2 : mode;
+    mode = (secondButton && firstButton) ? 0 : mode;
+
+    //delay(LONG_TIME_PAUSE);
+    switchLedLogika(mode);
+
   }
 
 
+  else{
 
-  
-  
- 
-  
+    mode = (firstButton) ? ((mode + 1) % 3) : mode;
+    mode = (secondButton) ? ((mode - 1 + 3) % 3) : mode;
+    mode = (secondButton && firstButton) ? 0 : mode;
+    switchLedLogika(mode);
 
-  delay(50);
+  }
+
+
+  delay(DEBOUNCE_TIME);
+}
+
+void switchLedLogika(char mode){
+  if (mode == 0)
+    {
+      blinking(LED_RED_PIN, LED_GREEN_PIN);
+    }
+    else if (mode == 1)
+    {
+      fast_blinking(LED_RED_PIN, LED_GREEN_PIN);
+    }
+
+    else if (mode == 2)
+    {
+      alarm(LED_RED_PIN, LED_GREEN_PIN);
+    }
 
 }
 
+void fast_blinking(int LED1, int LED2)
+{
+  for (int i = 0; i < 4; i++)
+  {
+    digitalWrite(LED1, HIGH);
+    delay(SHORT_TIME_PAUSE);
+    digitalWrite(LED1, LOW);
+    delay(SHORT_TIME_PAUSE);
+    digitalWrite(LED2, HIGH);
+    delay(SHORT_TIME_PAUSE);
+    digitalWrite(LED2, LOW);
+    delay(SHORT_TIME_PAUSE);
+  }
+}
+
+void alarm(int LED1, int LED2)
+{
+
+  digitalWrite(LED1, HIGH);
+  digitalWrite(LED2, HIGH);
+  delay(LONG_TIME_PAUSE);
+  digitalWrite(LED1, LOW);
+  digitalWrite(LED2, LOW);
+  delay(LONG_TIME_PAUSE);
+}
+
+void blinking(int LED1, int LED2)
+{
+
+  digitalWrite(LED1, HIGH);
+  delay(LONG_TIME_PAUSE);
+  digitalWrite(LED1, LOW);
+  digitalWrite(LED2, HIGH);
+  delay(LONG_TIME_PAUSE);
+  digitalWrite(LED2, LOW);
+}
