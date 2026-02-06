27d1673c12ac6c6704f6a9922f0b4cd0c28f1fe9
А.О. Печников <a.pechnikov@radiy.com>
2026-02-06 09:43:30 +0200

    First commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,54 +1,23 @@
 #include <Arduino.h>
 
-#define BUTTON_LEFT 15
-#define BUTTON_RIGHT 3
-
-volatile int16_t counter_left = 0;
-volatile int16_t counter_right = 0;
-
-volatile unsigned long lastPressTime = 0;
-volatile bool buttonState = HIGH;    // поточний стабільний стан
-volatile bool newEvent = false;      // сигнал про зміну стану кнопки
-
-void IRAM_ATTR reaction_left() {
-  counter_left++;
-}
-
-// ISR для правої кнопки
-void IRAM_ATTR reaction_right() {
-  unsigned long now = millis();
-  bool current = digitalRead(BUTTON_RIGHT);
-  counter_right++;
-  // антидребезг: ігноруємо зміни раніше, ніж через 50 мс
-  if (now - lastPressTime > 20) {
-    if (current != buttonState) {
-      buttonState = current;
-      newEvent = true;
-      lastPressTime = now;
-    }
-  }
-}
+#define ADC_PIN 18
+#define REF_VOLTAGE 3.3
+#define MAX_VALUE 4095
 
 void setup() {
-  pinMode(BUTTON_LEFT, INPUT);
-  pinMode(BUTTON_RIGHT, INPUT_PULLUP);
-  Serial.begin(115200);
-
-  attachInterrupt(digitalPinToInterrupt(BUTTON_LEFT), reaction_left, FALLING);
-  attachInterrupt(digitalPinToInterrupt(BUTTON_RIGHT), reaction_right, CHANGE);
+  Serial0.begin(115200);
+  analogSetAttenuation(ADC_11db);
 }
 
 void loop() {
-  if (newEvent) {
-    noInterrupts();
-    bool state = buttonState;
-    int16_t copy_counter_right = counter_right;
-    counter_right = 0;
-    newEvent = false;
-    interrupts();
+  int raw = analogRead(ADC_PIN);
+  float voltage = raw * REF_VOLTAGE / MAX_VALUE;
 
-    String position = (state == HIGH) ? "UNPRESS" : "PRESS";
-    Serial.println("\nRIGHT Button " + position + "! Count: " + String(copy_counter_right));
-    
-  }
+
+  float calibrVoltage = analogReadMilliVolts(ADC_PIN);
+
+  Serial0.println("Калібрована напруга:" + String(calibrVoltage));
+  Serial0.println("Напруга: " + String(voltage));
+  
+  delay(500);
 }
