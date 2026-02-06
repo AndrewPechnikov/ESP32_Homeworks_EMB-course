b8f5a709c3d25bc5f8289f7c2121aaee757df0c7
А.О. Печников <a.pechnikov@radiy.com>
2026-02-06 10:56:28 +0200

    second iteration

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,17 @@
 #include <Arduino.h>
 
 #define ADC_PIN 18
-#define REF_VOLTAGE 3.3
-#define MAX_VALUE 4095
+
+#define ADC_DB ADC_0db //ADC_2_5db, ADC_6db, ADC_11db)
+#define REF_VOLTAGE 0.8 // 1.1, 1.35, 3.3
+#define MAX_VALUE 511 // 1023, 2047, 4095
+#define BITS 9 // 10, 11, 12
+
 
 void setup() {
   Serial0.begin(115200);
-  analogSetAttenuation(ADC_11db);
+  analogSetAttenuation(ADC_DB);
+  analogReadResolution(BITS);
 }
 
 void loop() {
@@ -18,6 +23,7 @@ void loop() {
 
   Serial0.println("Калібрована напруга:" + String(calibrVoltage));
   Serial0.println("Напруга: " + String(voltage));
-  
-  delay(500);
+  int error = 100 - (voltage * 100 /(calibrVoltage/1000));
+  Serial0.println("Похибка:  " + String(error) + "%");
+  delay(1000);
 }
