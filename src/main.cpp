237082418b95bb71b07f789e9e3267814b932e66
Admin <User@CONSTRUKT-8.ae.com>
2026-02-08 23:26:43 +0200

    First iteration

--- src/main.cpp
+++ src/main.cpp
@@ -2,18 +2,21 @@
 #include <math.h>
 #define ADC_PIN 8
 
-#define ADC_DB ADC_11db //ADC_0db, ADC_2_5db, ADC_6db, ADC_11db)
-#define REF_VOLTAGE 3.3 // 0.8, 1.1, 1.35, 3.3
+#define LED_PIN 40
 
-#define BITS 12// 9, 10, 11, 12
+#define ADC_DB ADC_11db 
+#define REF_VOLTAGE 3.3 
+#define BITS 12
 
 
 
-const int maxValue = pow(2, BITS) - 1; 
+const float maxValue = pow(2, BITS) - 1; 
 void setup() {
   Serial0.begin(115200);
   analogSetAttenuation(ADC_DB);
   analogReadResolution(BITS);
+
+  pinMode(LED_PIN, OUTPUT);
 }
 
 void loop() {
@@ -21,11 +24,12 @@ void loop() {
   float voltage = raw * REF_VOLTAGE / maxValue;
 
 
-  float calibrVoltage = analogReadMilliVolts(ADC_PIN);
+  float PWM = raw / maxValue * 255;
 
-  Serial0.println("Калібрована напруга:" + String(calibrVoltage)+ "mV");
-  Serial0.println("Напруга: " + String(voltage) + "V");
-  int error = 100 - (voltage * 100 /(calibrVoltage/1000));
-  Serial0.println("Похибка:  " + String(error) + "%");
-  delay(1000);
+  analogWrite(LED_PIN, PWM);
+
+  
+  //Serial0.println("Напруга: " + String(PWM) + "%");
+  delay(1);
+  
 }
