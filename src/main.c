bfd0bc1b27594c64c285356d63a5d4a40f06f633
Admin <User@CONSTRUKT-8.ae.com>
2026-03-24 14:05:31 +0200

    format code

--- src/main.c
+++ src/main.c
@@ -8,7 +8,7 @@
 #include "esp_timer.h"
 
 // Конфігурація пінів
-#define ADC_PIN          ADC_CHANNEL_7   // GPIO8 на ESP32-S3
+#define ADC_PIN          ADC_CHANNEL_7
 #define ADC_UNIT         ADC_UNIT_1
 #define GPIO_LED1        GPIO_NUM_45
 #define GPIO_LED2        GPIO_NUM_37
@@ -16,75 +16,72 @@
 
 #define DEBOUNCE_TIME_MS 100
 
-
+// PWM конфігурація
 #define LEDC_MODE        LEDC_LOW_SPEED_MODE
 #define LEDC_TIMER       LEDC_TIMER_0
-#define LEDC_DUTY_RES    LEDC_TIMER_12_BIT 
+#define LEDC_DUTY_RES    LEDC_TIMER_12_BIT
 #define LEDC_FREQUENCY   5000
 
 volatile static int64_t last_interrupt_time = 0;
 volatile bool isLed = true;
 
-
-static void IRAM_ATTR gpio_isr_handler(void* arg) {
+static void IRAM_ATTR gpio_isr_handler(void *arg)
+{
     int64_t current_time = esp_timer_get_time() / 1000;
-    if (current_time - last_interrupt_time > DEBOUNCE_TIME_MS) {
-        isLed = !isLed; 
+    if (current_time - last_interrupt_time > DEBOUNCE_TIME_MS)
+    {
+        isLed = !isLed;
         last_interrupt_time = current_time;
     }
 }
 
-void init_dual_pwm() {
+void init_dual_pwm()
+{
     ledc_timer_config_t ledc_timer = {
-        .speed_mode       = LEDC_MODE,
-        .duty_resolution  = LEDC_DUTY_RES,
-        .timer_num        = LEDC_TIMER,
-        .freq_hz          = LEDC_FREQUENCY,
-        .clk_cfg          = LEDC_AUTO_CLK
+        .speed_mode = LEDC_MODE,
+        .duty_resolution = LEDC_DUTY_RES,
+        .timer_num = LEDC_TIMER,
+        .freq_hz = LEDC_FREQUENCY,
+        .clk_cfg = LEDC_AUTO_CLK
     };
     ledc_timer_config(&ledc_timer);
 
-
     ledc_channel_config_t chan0 = {
-        .speed_mode     = LEDC_MODE,
-        .channel        = LEDC_CHANNEL_0,
-        .timer_sel      = LEDC_TIMER,
-        .intr_type      = LEDC_INTR_DISABLE,
-        .gpio_num       = GPIO_LED1,
-        .duty           = 0,
-        .hpoint         = 0
+        .speed_mode = LEDC_MODE,
+        .channel = LEDC_CHANNEL_0,
+        .timer_sel = LEDC_TIMER,
+        .intr_type = LEDC_INTR_DISABLE,
+        .gpio_num = GPIO_LED1,
+        .duty = 0,
+        .hpoint = 0
     };
     ledc_channel_config(&chan0);
 
-
     ledc_channel_config_t chan1 = {
-        .speed_mode     = LEDC_MODE,
-        .channel        = LEDC_CHANNEL_1,
-        .timer_sel      = LEDC_TIMER,
-        .gpio_num       = GPIO_LED2,
-        .duty           = 0,
-        .hpoint         = 0
+        .speed_mode = LEDC_MODE,
+        .channel = LEDC_CHANNEL_1,
+        .timer_sel = LEDC_TIMER,
+        .gpio_num = GPIO_LED2,
+        .duty = 0,
+        .hpoint = 0
     };
     ledc_channel_config(&chan1);
 }
 
-void app_main() {
-
+void app_main()
+{
     init_dual_pwm();
-
 
     gpio_config_t io_conf = {
         .pin_bit_mask = (1ULL << GPIO_BUTTON),
         .mode = GPIO_MODE_INPUT,
         .pull_up_en = GPIO_PULLUP_ENABLE,
-        .intr_type = GPIO_INTR_NEGEDGE, 
+        .intr_type = GPIO_INTR_NEGEDGE,
     };
     gpio_config(&io_conf);
 
     gpio_install_isr_service(0);
-
-    gpio_isr_handler_add(GPIO_BUTTON, gpio_isr_handler, (void*) GPIO_BUTTON);
-
+    gpio_isr_handler_add(GPIO_BUTTON, gpio_isr_handler, (void *)GPIO_BUTTON);
 
     adc_oneshot_unit_handle_t adc_handle;
     adc_oneshot_unit_init_cfg_t init_config = {
@@ -97,27 +94,26 @@ void app_main() {
         .atten = ADC_ATTEN_DB_12,
     };
     adc_oneshot_config_channel(adc_handle, ADC_PIN, &config);
-
+    
     int adc_raw;
-    while (1) {
-        // 1. Читаємо актуальне значення з потенціометра
+    
+    while (1)
+    {
         ESP_ERROR_CHECK(adc_oneshot_read(adc_handle, ADC_PIN, &adc_raw));
-        uint32_t duty = adc_raw; // Масштабуємо під 13-біт PWM
-        if (isLed == 0) {
+        uint32_t duty = adc_raw;
         
+        if (isLed == 0)
+        {
             ledc_set_duty(LEDC_MODE, LEDC_CHANNEL_0, duty);
             ledc_update_duty(LEDC_MODE, LEDC_CHANNEL_0);
             ESP_LOGI("PWM", "Focus: LED 1, Duty: %lu", duty);
-        } 
-        else {
-     
+        }
+        else
+        {
             ledc_set_duty(LEDC_MODE, LEDC_CHANNEL_1, duty);
             ledc_update_duty(LEDC_MODE, LEDC_CHANNEL_1);
-            
             ESP_LOGI("PWM", "Focus: LED 2, Duty: %lu", duty);
         }
-
-     
         
         vTaskDelay(pdMS_TO_TICKS(50));
     }
