1e5bfb364186880f851696b94a202f5183db42ea
Admin <User@CONSTRUKT-8.ae.com>
2026-03-24 13:43:40 +0200

    final iteration

--- src/main.c
+++ src/main.c
@@ -1,109 +1,124 @@
-#include <freertos/FreeRTOS.h>
-#include <freertos/task.h>
-#include <esp_adc/adc_oneshot.h>
-#include <esp_log.h>
-#include <sdkconfig.h>
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_adc/adc_oneshot.h"
+#include "esp_log.h"
 #include "driver/gpio.h"
+#include "driver/ledc.h"
+#include "esp_timer.h"
+
+// Конфігурація пінів
+#define ADC_PIN          ADC_CHANNEL_7   // GPIO8 на ESP32-S3
+#define ADC_UNIT         ADC_UNIT_1
+#define GPIO_LED1        GPIO_NUM_45
+#define GPIO_LED2        GPIO_NUM_37
+#define GPIO_BUTTON      GPIO_NUM_5
+
+#define DEBOUNCE_TIME_MS 100
 
 
-#define ADC_PIN         ADC_CHANNEL_7   //gpio8
-#define ADC_unit        ADC_UNIT_1
-#define ADC_BITWIDTH    ADC_BITWIDTH_12
-#define ADC_ATTEN       ADC_ATTEN_DB_12
-#define WINDOW_SIZE     10
-#define GPIO_LED        GPIO_NUM_36
-#define ENABLE_LEVEL    2000
-#define HYST            100
+#define LEDC_MODE        LEDC_LOW_SPEED_MODE
+#define LEDC_TIMER       LEDC_TIMER_0
+#define LEDC_DUTY_RES    LEDC_TIMER_12_BIT 
+#define LEDC_FREQUENCY   5000
+
+volatile static int64_t last_interrupt_time = 0;
+volatile bool isLed = true;
 
 
-
-
-
-
-typedef struct {
-    int     values[WINDOW_SIZE];  
-    int     index;
-    long    sum;
-    bool    full;
-} sma_filter_t;
-
-void sma_init(sma_filter_t *f){
-    f -> index = 0;
-    f -> sum = 0;
-    f -> full = false;
-    for (int i = 0; i < WINDOW_SIZE; i++) f->values[i] = 0;
+static void IRAM_ATTR gpio_isr_handler(void* arg) {
+    int64_t current_time = esp_timer_get_time() / 1000;
+    if (current_time - last_interrupt_time > DEBOUNCE_TIME_MS) {
+        isLed = !isLed; 
+        last_interrupt_time = current_time;
+    }
 }
 
+void init_dual_pwm() {
+    ledc_timer_config_t ledc_timer = {
+        .speed_mode       = LEDC_MODE,
+        .duty_resolution  = LEDC_DUTY_RES,
+        .timer_num        = LEDC_TIMER,
+        .freq_hz          = LEDC_FREQUENCY,
+        .clk_cfg          = LEDC_AUTO_CLK
+    };
+    ledc_timer_config(&ledc_timer);
 
-int b_add_value (sma_filter_t *f, int new_value){
-    f->sum -= f->values[f->index];
-    f->values[f->index] = new_value;
-    f->sum += new_value;
-    f->index = (f->index + 1) % WINDOW_SIZE;
 
-    if (f->index == 0) f->full = true;
+    ledc_channel_config_t chan0 = {
+        .speed_mode     = LEDC_MODE,
+        .channel        = LEDC_CHANNEL_0,
+        .timer_sel      = LEDC_TIMER,
+        .intr_type      = LEDC_INTR_DISABLE,
+        .gpio_num       = GPIO_LED1,
+        .duty           = 0,
+        .hpoint         = 0
+    };
+    ledc_channel_config(&chan0);
 
-    return f->sum / (f->full ? WINDOW_SIZE : f->index);
+
+    ledc_channel_config_t chan1 = {
+        .speed_mode     = LEDC_MODE,
+        .channel        = LEDC_CHANNEL_1,
+        .timer_sel      = LEDC_TIMER,
+        .gpio_num       = GPIO_LED2,
+        .duty           = 0,
+        .hpoint         = 0
+    };
+    ledc_channel_config(&chan1);
 }
-
 
 void app_main() {
-    sma_filter_t sma_filter;
-    sma_init(&sma_filter);
+
+    init_dual_pwm();
+
 
     gpio_config_t io_conf = {
-        .pin_bit_mask = (1ULL << GPIO_LED),
-        .mode = GPIO_MODE_OUTPUT,
-        .pull_up_en = GPIO_PULLUP_DISABLE,
-        .pull_down_en = GPIO_PULLDOWN_DISABLE,
-        .intr_type = GPIO_INTR_DISABLE,
+        .pin_bit_mask = (1ULL << GPIO_BUTTON),
+        .mode = GPIO_MODE_INPUT,
+        .pull_up_en = GPIO_PULLUP_ENABLE,
+        .intr_type = GPIO_INTR_NEGEDGE, 
     };
-    ESP_ERROR_CHECK(gpio_config(&io_conf));
+    gpio_config(&io_conf);
+
+    gpio_install_isr_service(0);
+
+    gpio_isr_handler_add(GPIO_BUTTON, gpio_isr_handler, (void*) GPIO_BUTTON);
 
 
-
-
-
-    int adc_value;
     adc_oneshot_unit_handle_t adc_handle;
-
     adc_oneshot_unit_init_cfg_t init_config = {
-        .unit_id = ADC_unit,
-        .clk_src = ADC_RTC_CLK_SRC_DEFAULT,
+        .unit_id = ADC_UNIT,
     };
-
-    ESP_ERROR_CHECK(adc_oneshot_new_unit(&init_config, &adc_handle));
-
+    adc_oneshot_new_unit(&init_config, &adc_handle);
 
     adc_oneshot_chan_cfg_t config = {
-        .bitwidth = ADC_BITWIDTH,
-        .atten = ADC_ATTEN,
+        .bitwidth = ADC_BITWIDTH_12,
+        .atten = ADC_ATTEN_DB_12,
     };
+    adc_oneshot_config_channel(adc_handle, ADC_PIN, &config);
 
-    ESP_ERROR_CHECK(adc_oneshot_config_channel(adc_handle, ADC_PIN, &config));
-
-
-
-
-    while (1){
-
+    int adc_raw;
+    while (1) {
+        // 1. Читаємо актуальне значення з потенціометра
+        ESP_ERROR_CHECK(adc_oneshot_read(adc_handle, ADC_PIN, &adc_raw));
+        uint32_t duty = adc_raw; // Масштабуємо під 13-біт PWM
+        if (isLed == 0) {
         
-
-        ESP_ERROR_CHECK(adc_oneshot_read(adc_handle, ADC_PIN, &adc_value));
-
-        int filtered_val = b_add_value(&sma_filter, adc_value);
-
-
-        ESP_LOGI("ADC", "Raw: %d | Filtered: %d", adc_value, filtered_val);
-
-        vTaskDelay(pdMS_TO_TICKS(100));
-
-        if(filtered_val > ENABLE_LEVEL){
-            gpio_set_level(GPIO_LED, 1);
+            ledc_set_duty(LEDC_MODE, LEDC_CHANNEL_0, duty);
+            ledc_update_duty(LEDC_MODE, LEDC_CHANNEL_0);
+            ESP_LOGI("PWM", "Focus: LED 1, Duty: %lu", duty);
+        } 
+        else {
+     
+            ledc_set_duty(LEDC_MODE, LEDC_CHANNEL_1, duty);
+            ledc_update_duty(LEDC_MODE, LEDC_CHANNEL_1);
+            
+            ESP_LOGI("PWM", "Focus: LED 2, Duty: %lu", duty);
         }
 
-        else if(filtered_val < (ENABLE_LEVEL - HYST)){
-            gpio_set_level(GPIO_LED, 0);
-        }    
+     
+        
+        vTaskDelay(pdMS_TO_TICKS(50));
     }
 }
